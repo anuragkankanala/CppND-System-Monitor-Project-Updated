1aceef899caa437684791f9e3e70f5315aedc5dd
Anurag Kankanala <anuragk.1005@gmail.com>
2020-04-25 12:38:07 +0530

    implement process

--- include/process.h
+++ include/process.h
@@ -2,12 +2,14 @@
 #define PROCESS_H
 
 #include <string>
+#include <unistd.h>
 /*
 Basic class for Process representation
 It contains relevant attributes as shown below
 */
 class Process {
  public:
+  Process(int pid);
   int Pid();                               // TODO: See src/process.cpp
   std::string User();                      // TODO: See src/process.cpp
   std::string Command();                   // TODO: See src/process.cpp
@@ -18,6 +20,7 @@ class Process {
 
   // TODO: Declare any necessary private members
  private:
+ 	int pid_{-1};
 };
 
 #endif
--- src/process.cpp
+++ src/process.cpp
@@ -5,29 +5,101 @@
 #include <vector>
 
 #include "process.h"
+#include "linux_parser.h"
 
 using std::string;
 using std::to_string;
 using std::vector;
 
+Process::Process(int pid)
+{
+	if(pid > 0)
+	{
+		pid_ = pid;
+	}
+}
 // TODO: Return this process's ID
-int Process::Pid() { return 0; }
+int Process::Pid() const { return pid_; }
 
 // TODO: Return this process's CPU utilization
-float Process::CpuUtilization() { return 0; }
+float Process::CpuUtilization() 
+{
+	//https://stackoverflow.com/questions/16726779/how-do-i-get-the-total-cpu-usage-of-an-application-from-proc-pid-stat/16736599#16736599
+	int pid = Pid();
+	float cpu_usage{0};
+	if(pid > 0)
+	{
+		long process_active_jiffies{LinuxParser::ActiveJiffies(pid)};
+		long process_active_seconds = process_active_jiffies/sysconf(_SC_CLK_TCK);
+
+		long system_uptime_seconds{LinuxParser::UpTime()};
+
+		long process_uptime_jiffies{LinuxParser::UpTime(pid)};
+		long process_uptime_seconds = process_uptime_jiffies/sysconf(_SC_CLK_TCK);
+
+		long time_elapsed_process_start = system_uptime_seconds - process_uptime_seconds;
+
+		cpu_usage = ((100 * process_active_seconds)/(time_elapsed_process_start));
+
+	}
+
+	return cpu_usage;
+}
 
 // TODO: Return the command that generated this process
-string Process::Command() { return string(); }
+string Process::Command() 
+{
+	string command{""};
+	int pid = Pid();
+	if(pid > 0)
+	{
+		command = LinuxParser::Command(pid);
+	}
+
+	return command;
+}
 
 // TODO: Return this process's memory utilization
-string Process::Ram() { return string(); }
+string Process::Ram() 
+{
+	string ram{""};
+	int pid = Pid();
+	if(pid > 0)
+	{
+		ram = LinuxParser::Ram(pid);
+	}
+
+	return ram;
+}
 
 // TODO: Return the user (name) that generated this process
-string Process::User() { return string(); }
+string Process::User()
+{
+	string user{""};
+	int pid = Pid();
+	if(pid > 0)
+	{
+		user = LinuxParser::User(pid);
+	}
+
+	return user;
+}
 
 // TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return 0; }
+long int Process::UpTime() {
+	int pid = Pid();
+	long int uptime{0};
+	if(pid > 0)
+	{
+		uptime = LinuxParser::UpTime(pid);
+	}
+
+	return uptime;
+}
 
 // TODO: Overload the "less than" comparison operator for Process objects
 // REMOVE: [[maybe_unused]] once you define the function
-bool Process::operator<(Process const& a[[maybe_unused]]) const { return true; }
+bool Process::operator<(Process const& a) const 
+{
+	return (CpuUtilization() < a.CpuUtilization());
+}
