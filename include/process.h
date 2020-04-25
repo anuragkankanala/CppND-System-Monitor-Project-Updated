4a383c118f721612ed4d7bc6a4d570808bfeb72f
Anurag Kankanala <anuragk.1005@gmail.com>
2020-04-25 12:41:54 +0530

    Implement processor

--- include/process.h
+++ include/process.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <unistd.h>
+#include "linux_parser.h"
 /*
 Basic class for Process representation
 It contains relevant attributes as shown below
--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,15 @@
 #include "processor.h"
 
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization() 
+{
+	long total_system_jiffies{LinuxParser::Jiffies()};
+	long active_system_jiffies{LinuxParser::ActiveJiffies()};
+
+	if(total_system_jiffies > 0)
+	{
+		return ((active_system_jiffies)/total_system_jiffies);
+	}
+
+	return 0;
+}
